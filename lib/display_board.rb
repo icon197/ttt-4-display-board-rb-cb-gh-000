52df60727c246661810577cf9cdd148e6549ce20
Phan Huynh Tan <tanb1509892@student.ctu.edu.vn>
2019-01-05 16:16:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(arr)
+  puts " #{arr[0]} | #{arr[1]} | #{arr[2]} "
   puts "-----------"
+  puts " #{arr[0]} | #{arr[1]} | #{arr[2]} "
   puts "-----------"
-  puts "-----------"
-  puts "-----------"
-  puts "-----------"
+  puts " #{arr[0]} | #{arr[1]} | #{arr[2]} "
 end
